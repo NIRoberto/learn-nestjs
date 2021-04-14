d1e3e778304fd5f6c40778aa39e0478812638759
niroberto <robertwilly668@gmail.com>
2021-04-14 18:48:57 +0200

    create simple CRUD for tasks

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateTaskDto } from 'src/DTO/create-task.dot';
+import { Task, TaskStatus } from './tasks.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.taskService.createTask(createTaskDto);
+  }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.taskService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    this.taskService.deleteTaskById(id);
+  }
+
+  @Patch('/:id/status')
+  updateByIdTask(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.taskService.updateById(id, status);
+  }
+}
